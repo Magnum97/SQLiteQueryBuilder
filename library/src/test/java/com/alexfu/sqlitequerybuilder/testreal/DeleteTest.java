da84f66c965780c3f3e72ee590be6b31a1f00e0e
Alex Fu <alex.fu914@gmail.com>
2016-04-07 07:31:55 -0400

    test: Create DeleteTest

---
+++ library/src/test/java/com/alexfu/sqlitequerybuilder/testreal/DeleteTest.java
@@ -0,0 +1,66 @@
+package com.alexfu.sqlitequerybuilder.testreal;
+
+import com.alexfu.sqlitequerybuilder.api.SQLiteQueryBuilder;
+
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.alexfu.sqlitequerybuilder.api.SQLiteClauseBuilder.clause;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeleteTest extends SQLiteTest {
+
+  @Override
+  public void before() throws SQLException {
+    super.before();
+
+    // Seed database
+
+    String createTable = "CREATE TABLE my_table (id INTEGER)";
+    statement.execute(createTable);
+
+    for (int i = 1; i <= 10; i++) {
+      String sql = "INSERT INTO my_table (id) VALUES (" + i + ")";
+      statement.execute(sql);
+    }
+  }
+
+  @Test
+  public void testDeleteAll() throws SQLException {
+    // Arrange
+    String sql = SQLiteQueryBuilder
+        .delete()
+        .from("my_table")
+        .build();
+
+    // Act
+    statement.execute(sql);
+
+    // Assert
+    ResultSet set = statement.executeQuery("SELECT * FROM my_table");
+    int rowCount = 0;
+    while (set.next()) { rowCount++; }
+    assertThat(rowCount).isEqualTo(0);
+  }
+
+  @Test
+  public void testDeleteWhere() throws SQLException {
+    // Arrange
+    String sql = SQLiteQueryBuilder
+        .delete()
+        .from("my_table")
+        .where(clause("id = 1"))
+        .build();
+
+    // Act
+    statement.execute(sql);
+
+    // Assert
+    ResultSet set = statement.executeQuery("SELECT * FROM my_table WHERE id = 1");
+    int rowCount = 0;
+    while (set.next()) { rowCount++; }
+    assertThat(rowCount).isEqualTo(0);
+  }
+}
